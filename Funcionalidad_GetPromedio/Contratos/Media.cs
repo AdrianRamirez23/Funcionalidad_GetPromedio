1d7f435a3ee00505d955480ef1ef67cf5288fd3e
AdrianRamirez23 <adrian.ramirez23@hotmail.com>
2023-03-19 19:06:36 -0500

    Add project files.

---
+++ Funcionalidad_GetPromedio/Contratos/Media.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionalidad_GetPromedio.Contratos
+{
+    public class Media
+    {
+        public List<int> numberRandom(int n) 
+        {
+            var seed = Environment.TickCount;
+            var random = new Random(seed);
+            var list = new List<int>();
+            for (int i = 0; i < n; i++) 
+            {
+                list.Add( random.Next(0, 9));
+            }
+            return list.Order().ToList();
+        }
+        public double GetMedia(string agent, List<int> numbers) 
+        {
+            switch (agent) 
+            {
+                case "A":
+                    return GetAverage(numbers);
+                    break;
+                case "B":
+                    return GetArmonicMedia(numbers);
+                    break; 
+                case "C":
+                    return GetMedian(numbers);
+                    break;
+                default:
+                    return 0;
+                    break;
+            }
+        }
+        public string ResultMethod(string agent) 
+        {
+            switch (agent)
+            {
+                case "A":
+                    return "Promedio";
+                    break;
+                case "B":
+                    return "Media Amornica";
+                    break;
+                case "C":
+                    return "Mediana";
+                    break;
+                default:
+                    return null;
+                    break;
+            }
+        }
+        internal double GetAverage(List<int> numbers) 
+        {
+            return numbers.Average();
+        }
+        internal double GetArmonicMedia(List<int> numbers) 
+        {
+            var mediaArmonic = 0;
+            for (int i = 1; i < numbers.Count(); i++)
+            {
+                
+                mediaArmonic = mediaArmonic + 1/ numbers[i];
+            }
+            return numbers.Count() / mediaArmonic;
+        }
+        internal double GetMedian(List<int> numbers) 
+        {
+            int pos = numbers.Count() / 2;
+            if (numbers.Count() % 2 == 0)
+            {
+                return (numbers[pos] + numbers[pos + 1]) / 2;
+            }
+            else
+            {
+                return numbers[pos];
+            }
+
+        }
+        
+    }
+}
---
+++ Funcionalidad_GetPromedio/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Funcionalidad_GetPromedio.Contratos;
+
+List<string> listAgents = new List<string>() { "A", "B", "C" };
+string agent;
+List<int> numbers = null;
+int size = 0;
+
+Console.WriteLine("Seleccione A) Agente A. B) Agente B. C) Agente C: ");
+agent = Console.ReadLine();
+
+if(listAgents.Exists(a => a  == agent.ToUpper())) 
+{
+    Console.WriteLine("Ingrese el tamaño de lista de números (debe ser mayor a 0 y menor o igual a 100): ");
+    int.TryParse(Console.ReadLine(), out size);
+    if(size > 0 && size <= 100) 
+    {
+        numbers = new Media().numberRandom(size);
+        Console.WriteLine("La lista autogenerada es: ");
+        for(int i = 0; i < numbers.Count(); i++) 
+        {
+            if(i == numbers.Count() - 1)
+                Console.WriteLine(numbers[i]+". ");
+            else
+                Console.Write(numbers[i]+", ");
+        }
+        Console.WriteLine("El agente seleccionado " + agent.ToUpper() + ", usó el metodo: " + new Media().ResultMethod(agent.ToUpper()) + ", y su resultado es: " + new Media().GetMedia(agent.ToUpper(), numbers));
+    }
+    else 
+    {
+        Console.WriteLine("No elegiste un tamaño válido.");
+    }
+}
+else 
+{
+    Console.WriteLine("No elegiste un agente válido.");
+}
+
+Console.ReadKey();
+
+
+
+
